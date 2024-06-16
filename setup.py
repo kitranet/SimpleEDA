6b491566fca6d3d383a81d7d9282fd431fd3bb19
vijaykrishnanmr <vijaykrishnanmr@gmail.com>
2024-06-16 18:27:37 +0530

    Update setup.py with correct package name and URL

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='SimplyEDA',  # Ensure this name is unique on PyPI
-    version='0.1.2',
+    version='0.1.3',
     description='A simple library for exploratory data analysis',
     long_description=README,
     long_description_content_type='text/markdown',
